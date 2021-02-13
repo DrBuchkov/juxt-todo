c40dae5c2359db5475c4c565216a40c8ed8c23e7
Nikolas Pafitis <silencioseu@gmail.com>
2021-02-13 16:54:35 +0200

    Cleanup main.

--- src/clj/pro/juxt/core.clj
+++ src/clj/pro/juxt/core.clj
@@ -1,8 +1,5 @@
 (ns pro.juxt.core
-  (:require [mount.core :as mount]
-            [ring.adapter.jetty :refer [run-jetty]]
-            [pro.juxt.http.core :refer [handler]])
   (:gen-class))
 
 (defn -main [& args]
-  (mount/start))
+  (println "Hello World!"))
